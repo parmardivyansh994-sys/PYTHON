cc6cd099cec15680dc601273fbb5313474c1e77e
parmardivyansh994-sys <parmardivyansh994@gmail.com>
2026-01-07 18:06:35 -0800

    Add files via upload

---
+++ python/9_tupple2.py
@@ -0,0 +1,12 @@
+'''
+tupl 2
+'''
+
+
+states = ("gujarat","madhaya Pradesh","keral","uttar pradhesh","Chhattisgadh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal")
+
+print(states)
+print("Position of gujarat ",states.index('keral'))
+# print("Position of gujarat ",states.index('Delhi')) #value not found is error
+print("Count of keral ",states.count('keral')) #1
+print("Count of Delhi ",states.count('Delhi')) #0
